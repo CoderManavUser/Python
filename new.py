caa0b499f535617699d5579902042dd84b3ed769
CoderManavUser <123233266+CoderManavUser@users.noreply.github.com>
2023-02-26 12:21:54 +0400

    Add files via upload

---
+++ new.py
@@ -0,0 +1,13 @@
+n1=5
+n2=6
+sum=n1+n2
+subtract=n1-n2
+multiply=n1*n2
+divide=n1/n2
+remainder=n2%n1
+#this is a calculator program
+print("The sum of 5 and 6 is", sum)
+print("The difference of 5 and 6 is", subtract)
+print("The product of 5 and 6 is", multiply)
+print("The division of 5 and 6 is", divide)
+print("The remainder of 5 and 6 is", remainder)
